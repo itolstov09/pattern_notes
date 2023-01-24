093ecf20fc6db4a94dc98d6c294398c0c708e24f
itolstov09 <itolstov09@gmail.com>
2023-01-24 12:15:19 +0600

    feat: creational - singleton

---
+++ src/com/company/creational/Singleton.java
@@ -0,0 +1,25 @@
+package com.company.creational;
+
+import java.util.Objects;
+
+public final class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+
+    public static void main(String[] args) {
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+        System.out.println(Objects.equals(instance1, instance2));
+    }
+
+}
